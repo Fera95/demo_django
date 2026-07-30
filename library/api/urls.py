baee0c68dcf574b152da8d3b2824e06229c74978
Fera95 <ferarios95@gmail.com>
2026-07-29 19:05:48 -0600

    feat: se tiene get y post , con serializer

--- library/api/urls.py
+++ library/api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import BookListCreateView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view()),
-    path('books/<int:id>', BookDetailView.as_view()),
+    #path('books/<int:id>', BookDetailView.as_view()),
 ]
--- library/api/views.py
+++ library/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookListCreateView(APIView):
+    def get(self,request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books,many=True)
+        return Response({"status":"sucess", "data":serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"status":"sucess","data":serializer.data},
+                status=status.HTTP_201_CREATED
+            )
+        #Si no es valido
+        return Response({"status":"error","data": serializer.errors},status=status.HTTP_400_BAD_REQUEST)
